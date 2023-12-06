3f657f45d12dea1feafb77690db09670d87279ef
Gustav Lillesæter <gustav.lillesater@gmail.com>
2023-12-06 13:21:59 +0100

    day 6 init

---
+++ aoc2023/src/day06/index.ts
@@ -0,0 +1,41 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => {
+  console.log(rawInput.split(":")[1]);
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `Time:      7  15   30
+Distance:  9  40  200`,
+        expected: 288,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
