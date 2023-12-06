8c255f9338b7a21e21e174c2e78f3ebff14e6947
Gustav Lillesæter <gustav.lillesater@gmail.com>
2023-12-06 14:52:41 +0100

    day 6

--- aoc2023/src/day06/index.ts
+++ aoc2023/src/day06/index.ts
@@ -1,19 +1,57 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => {
-  console.log(rawInput.split(":")[1]);
+type Race = {
+  time: number;
+  recordDistance: number;
+}
+
+const parseInputPart1 = (rawInput: string): Race[] => {
+  var [times, distances] = rawInput.split("\n").map(line => line.split(":")[1].trim().split(/\s+/).map(Number))
+  if (times.length !== distances.length) throw new Error("parsing failed")
+
+  var races: Race[] = times.map((time, idx) => {
+    return {
+      time: time,
+      recordDistance: distances[idx]
+    }
+  })
+  return races;
 };
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const parseInputPart2 = (rawInput: string): Race => {
+  var [time, distance] = rawInput.split("\n").map(line => line.split(":")[1].trim().replace(/\s/g, "")).map(Number)
 
-  return;
+  return { time: time, recordDistance: distance }
+};
+
+const caluclateDistance = (speed: number, time: number) => speed * time
+
+const part1 = (rawInput: string) => {
+  const races: Race[] = parseInputPart1(rawInput);
+
+  var waysToWinPerRace: number[] = Array(races.length).fill(0)
+
+  races.forEach((race, idx) => {
+    [...Array(race.time + 1).keys()].forEach(windUpTime => {
+      const distance = caluclateDistance(windUpTime, race.time - windUpTime)
+      if (distance > race.recordDistance) waysToWinPerRace[idx]++
+    });
+  })
+
+  return waysToWinPerRace.reduce((prev, cur) => prev * cur, 1)
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const race: Race = parseInputPart2(rawInput);
 
-  return;
+  var waysToWin: number = 0;
+
+  [...Array(race.time + 1).keys()].forEach(windUpTime => {
+    const distance = caluclateDistance(windUpTime, race.time - windUpTime)
+    if (distance > race.recordDistance) waysToWin++
+  });
+
+  return waysToWin;
 };
 
 run({
@@ -29,10 +67,11 @@ Distance:  9  40  200`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Time:      7  15   30
+Distance:  9  40  200`,
+        expected: 71503,
+      },
     ],
     solution: part2,
   },
